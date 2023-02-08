7583988cfc9e561ed44479018dc0e7897bf43521
LuisAguero2023 <124743845+LuisAguero2023@users.noreply.github.com>
2023-02-07 22:10:04 -0300

    Se instala NodeJs al Proyecto

---
+++ Entrega_N-1_CV_Luis_Jesus_Aguero/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Entrega_N-1_CV_Luis_Jesus_Aguero';
+}
